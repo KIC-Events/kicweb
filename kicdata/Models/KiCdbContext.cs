4629c6629a98d963891047df657cc9230033afa0
Michael Gallagher <gallagher.michaeljr@gmail.com>
2024-04-08 11:20:15 -0400

    Updated Data Model

---
+++ kicdata/Models/KiCdbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace KiCData.Models
+{
+    public class KiCdbContext : DbContext
+    {
+        public DbSet<Vendor> Vendors { get; set; }
+        public DbSet<Volunteer> Volunteers { get; set; }
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Presenter> Presenters { get; set; }
+
+        public string DbPath { get; }
+
+        public KiCdbContext()
+        {
+            DbPath = string.Empty;
+        }
+    }
+}
---
+++ kicdata/Models/Presenter.cs
@@ -0,0 +1,13 @@
+﻿namespace KiCData.Models
+{
+    public class Presenter
+    {
+        public string? FetName { get; set; }
+
+        public string? CourseDescription { get; set; }
+
+        public DateOnly? LastAttended { get; set; }
+
+        public string? Topics { get; set; }
+    }
+}
