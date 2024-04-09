fe1892d6dddf3c0a9515eefea6296f6cce653c8a
Garrett Kelly <gckelly761@gmail.com>
2024-04-08 20:48:36 -0400

    Adds config file

--- kicweb/Program.cs
+++ kicweb/Program.cs
@@ -8,13 +8,18 @@ IConfigurationRoot config = configBuilder.Build();
 
 // Add services to the container.
 builder.Services.AddSingleton(config);
-builder.Services.AddSingleton<IEmailService, EmailService>(config);
+builder.Services.AddSingleton<IEmailService, EmailService>();
 builder.Services.AddSingleton<IUserService, UserService>();
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 builder.Services.AddSingleton<ICookieService, CookieService>();
 builder.Services.AddControllersWithViews();
 
 WebApplication app = builder.Build();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+	ForwardedHeaders = Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedFor | Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedProto
+});
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
