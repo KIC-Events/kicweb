1de46ae0e75416e44b6b2bb16041033cee7dc37d
Michael Gallagher <gallagher.michaeljr@gmail.com>
2024-07-14 12:36:31 -0400

    Added User, Staff, and ClubMember to the data model for scaffolding

---
+++ kicdata/Models/ClubMember.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KiCData.Models
+{
+    public class ClubMember
+    {
+        [Key]
+        public Guid? Id { get; set; }
+        [Required]
+        public int ClubId { get; set; }
+        [Required]
+        public string? FirstName { get; set; }
+        [Required]
+        public string? LastName { get; set; }
+        [Required]
+        public DateOnly? DateOfBirth { get; set; }
+        [Required]  
+        public string? Sex { get; set; }
+    }
+}
---
+++ kicdata/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KiCData.Models
+{
+    public class User : Member
+    {
+        [Key]
+        public Guid? Id { get; set; }
+
+        public Guid? MemberId { get; set; }
+        public virtual Member? Member { get; set; }
+
+        [Required]
+        public string? Username { get; set; }
+        [Required]
+        public string? Password { get; set; }
+
+    }
+}
