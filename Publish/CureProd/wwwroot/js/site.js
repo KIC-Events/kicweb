1413e1707af0d1fec43b7a4b1052445bfdc5019e
malechus <garrett@malechus.com>
2024-11-22 10:29:50 -0500

    Updates proj and pub for cure again

---
+++ Publish/CureProd/wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function showStates() {
+    var states = ["Arkansas", "California", "Colorado", "Connecticut","District of Columbia", "Hawaii", "Maine", "Maryland", "Massachusetts",
+        "Michigan", "Minnesota", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "Oregon", "Pennsylvania", "Rhode Island", "Vermont", "Virginia", "Washington"];
+
+    $('#state').on('change', function () {
+        if (states.includes($(this).val())) {
+            $('#IdOther').show();
+        }
+    })
+    
+}
